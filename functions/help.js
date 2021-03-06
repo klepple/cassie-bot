9722023d3514d2bb8edcf85bd3a24201c7666525
Kahlia Lepple <klepple@uwo.ca>
2021-03-06 01:23:50 -0500

    Update help function

--- functions/help.js
+++ functions/help.js
@@ -6,6 +6,7 @@ module.exports = {
         title = "✨ What would you like to hear about today? ✨"
         // Commands
         advice = "~advice: I will consult the stars and give you some advice!"
+        stars = "~stars: I will ask the stars to rate the categories of your life."
         help = "~help: I will repeat this again uwu"
 
         msg.channel.send(surround + "\n" + title + "\n" + advice + "\n" + help + "\n" + surround)
